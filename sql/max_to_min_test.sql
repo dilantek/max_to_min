067d2a8b6287011976ddbecd443d72d521a800cf
dilantek <66736240+dilantek@users.noreply.github.com>
2024-01-10 19:29:50 +0300

    Update max_to_min_test.sql

--- sql/max_to_min_test.sql
+++ sql/max_to_min_test.sql
@@ -1,1 +1,9 @@
-
+select max_to_min(gs::smallint),
+max_to_min(gs::integer),
+max_to_min(gs::bigint),
+max_to_min(gs::numeric)
+from (
+select gs
+from (
+select generate_series(1,10) as gs 
+) t1 ) t1 ;
